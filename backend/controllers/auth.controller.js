78b163282481b93912a2d4a4b195eea77624af41
unknown <yuvrajsingh.ongraph@example.com>
2023-12-04 11:27:41 +0530

    fix: format the code in auth routes and controllers folder in backend

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -11,10 +11,19 @@ const { sendMail } = require("../utils/sendMail.utils");
 const ejs = require("ejs");
 const fs = require("fs");
 const path = require("path");
+const {google} = require("googleapis");
+const dayjs = require("dayjs");
 // const UniqueStringGenerator = require("unique-string-generator");
 const tokenSet = new Set();
 // const randomToken = UniqueStringGenerator.UniqueString();
 // let Token ;
+
+
+const oauth2Client = new google.auth.OAuth2(
+  config.GOOGLE_CONTACT_CLIENT_ID,
+  config.GOOGLE_CONTACT_CLIENT_SECRET,
+  config.GOOGLE_REDIRECT_URI
+);
 
 // @desc    Signup user
 // @route   POST /api/auth/signup
@@ -326,19 +335,237 @@ const userResetPassword = asyncHandler(async (req, res) => {
   );
 });
 
-const googleAuthSuccess = (req, res) => {
-  console.log(req.user);
+
+const googleAuth = asyncHandler(async(req,res)=>{
+  const authUrl = oauth2Client.generateAuthUrl({
+    access_type: "offline",
+    scope: [
+      "https://www.googleapis.com/auth/contacts.readonly",
+      "https://www.googleapis.com/auth/calendar",
+      "https://www.googleapis.com/auth/userinfo.email",
+      "https://www.googleapis.com/auth/userinfo.profile",
+    ], // Scope for accessing contacts
+  });
+  res.redirect(authUrl);
+})
+
+const googleAuthRedirect = asyncHandler(async(req,res)=>{
+  const code = req.query.code;
+  const { tokens } = await oauth2Client.getToken(code);
+
+  // Set access token to authorize API requests
+  oauth2Client.setCredentials(tokens);
+
+  return res.redirect(
+    `http://127.0.0.1:5173/google?oauth2Client=${JSON.stringify(oauth2Client)}`
+  );
+})
+
+const googleOauthUser = asyncHandler(async(req,res)=>{
+  if (!oauth2Client.credentials.refresh_token) {
+    return commonErrorHandler(
+      req,
+      res,
+      null,
+      404,
+      "Please, Sync with the Google first to see your contacts"
+    );
+  }
+  const oauth2 = google.oauth2({ version: "v2", auth: oauth2Client });
+  const userInfo = await oauth2.userinfo.get();
+  const body = userInfo.data;
+  const userWithEmail = await User.findOne({ email: body.email });
+
+  if (body?.email === userWithEmail?.email) {
+    const token = jwt.sign({ _id: userWithEmail._id }, config.JWT_SECRET);
+    return commonErrorHandler(
+      req,
+      res,
+      { data: {token:token, details: userWithEmail}, quote: "User sign in with google successfully" },
+      200
+    );
+    }else{
+  body.password = jwt.sign({ password: body.password }, config.JWT_SECRET);
+  const user = new User({name:body.name,email:body.email,password:body.password,isRegister:true,url:body.picture});
+  const data = await user.save();
+  const token = jwt.sign({ _id: data._id }, config.JWT_SECRET);
   return commonErrorHandler(
     req,
     res,
-    { data: req.user.displayName, quote: "User login success by google" },
-    202
+    { data: {token:token, details:data}, quote: "User sign in with google successfully" },
+    200
   );
-};
+  }
+})
 
-const googleAuthFailure = asyncHandler(async (req, res) => {
-  return res.send("Fail authentication!!");
+const googleContacts = asyncHandler(async(req,res)=>{
+  if (!oauth2Client.credentials.refresh_token) {
+    return commonErrorHandler(
+      req,
+      res,
+      null,
+      404,
+      "Please, Sync with the Google first to see your contacts"
+    );
+  }
+  const peopleApi = google.people({
+    version: "v1",
+    auth: oauth2Client,
+  });
+
+  try {
+    // Retrieve a list of contacts
+    const contacts = await peopleApi.people.connections.list({
+      resourceName: "people/me",
+      personFields: "names,emailAddresses", // Fields to retrieve (you can add more fields)
+    });
+
+    // Return contacts data
+    const contactList = contacts.data.connections.map((contact) => {
+      return contact.names[0].displayName;
+    });
+
+    return commonErrorHandler(req, res, { data: contactList, quote: "" }, 200);
+  } catch (error) {
+    console.error("Error fetching contacts:", error.message);
+    res.status(500).send("Error fetching contacts");
+  }
+})
+
+const calendar = google.calendar({
+  version: "v3",
+  auth: config.CALENDAR_API_KEY,
 });
+
+const createGoogleCalendarEvent = asyncHandler(async(req,res)=>{
+  if (!oauth2Client.credentials.refresh_token) {
+    return commonErrorHandler(
+      req,
+      res,
+      null,
+      404,
+      "Please, Sync with the Google first to create your event in google calendar too"
+    );
+  }
+  const body = req.body;
+
+  const createdEvent = await calendar.events.insert({
+    calendarId: "primary",
+    auth: oauth2Client,
+    requestBody: {
+      summary: body.title,
+      description: "It is a very important event",
+      start: {
+        dateTime: dayjs(new Date(body.startTime))
+          .add(5, "hours")
+          .add(30, "minutes")
+          .toISOString(),
+        timeZone: "Asia/Kolkata",
+      },
+      end: {
+        dateTime: dayjs(new Date(body.endTime))
+          .add(5, "hours")
+          .add(30, "minutes")
+          .toISOString(),
+        timeZone: "Asia/Kolkata",
+      },
+    },
+  });
+  return commonErrorHandler(
+    req,
+    res,
+    {
+      data: { id: createdEvent.data.id },
+      quote: "Event created in google calendar successfully",
+    },
+    200
+  );
+})
+
+const deleteGoogleCalendarEvent = asyncHandler(async(req,res)=>{
+  if (!oauth2Client.credentials.refresh_token) {
+    return commonErrorHandler(
+      req,
+      res,
+      null,
+      404,
+      "Please, Sync with the Google first to create your event in google calendar too"
+    );
+  }
+
+  calendar.events.delete(
+    {
+      calendarId: "primary",
+      eventId: req.params.id,
+      auth: oauth2Client,
+    },
+    (err, response) => {
+      if (err) {
+        console.error("Error deleting event:", err);
+        return;
+      }
+      console.log("Event deleted:");
+    }
+  );
+  return commonErrorHandler(
+    req,
+    res,
+    { data: {}, quote: "Event deleted in google calendar successfully" },
+    200
+  );
+})
+
+const updateGoogleCalendarEvent = asyncHandler(async(req,res)=>{
+  if (!oauth2Client.credentials.refresh_token) {
+    return commonErrorHandler(
+      req,
+      res,
+      null,
+      404,
+      "Please, Sync with the Google first to create your event in google calendar too"
+    );
+  }
+  const body = req.body;
+
+  calendar.events.update(
+    {
+      calendarId: "primary",
+      eventId: req.params.id,
+      auth: oauth2Client,
+      requestBody: {
+        summary: body.title,
+        description: "It is a very important event",
+        start: {
+          dateTime: dayjs(new Date(body.startTime))
+            .add(5, "hours")
+            .add(30, "minutes")
+            .toISOString(),
+          timeZone: "Asia/Kolkata",
+        },
+        end: {
+          dateTime: dayjs(new Date(body.endTime))
+            .add(5, "hours")
+            .add(30, "minutes")
+            .toISOString(),
+          timeZone: "Asia/Kolkata",
+        },
+      },
+    },
+    (err, response) => {
+      if (err) {
+        console.error("Error updating event:", err);
+        return;
+      }
+      console.log("Event updated:");
+    }
+  );
+  return commonErrorHandler(
+    req,
+    res,
+    { data: {}, quote: "Event updated in google calendar successfully" },
+    200
+  );
+})
 
 module.exports = {
   userSignUp,
@@ -347,7 +574,12 @@ module.exports = {
   userSignUpVerification,
   userResetPassword,
   userResetPasswordMail,
-  googleAuthSuccess,
-  googleAuthFailure,
   userGoogleSignIn,
+  googleAuth,
+  googleAuthRedirect,
+  googleOauthUser,
+  googleContacts,
+  createGoogleCalendarEvent,
+  deleteGoogleCalendarEvent,
+  updateGoogleCalendarEvent
 };
