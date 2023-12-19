fbc88f3aac4203cc22e32d653cdf0837d2692117
unknown <yuvrajsingh.ongraph@example.com>
2023-12-19 12:00:05 +0530

    fix: changes in vite configuration

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -16,6 +16,8 @@ export default defineConfig({
         manualChunks: {
           'react-query': ['react-query'],
           'mui': ['@mui/material', '@mui/icons-material'],
+          'vendor': ['react', 'react-dom','react-redux','react-modal', 'react-router-dom','react-image-crop','react-error-boundary','react-cookie','react-big-calendar'],
+          'other-dependencies': ['axios', 'moment','dayjs'],
         },
       },
     },
